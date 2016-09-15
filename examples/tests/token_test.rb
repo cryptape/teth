9af13da1bf54582f7d07db5630cc936ae4b94b0a
Jan Xie <jan.h.xie@gmail.com>
2016-09-15 16:27:30 +0800

    auto setup

--- examples/tests/token_test.rb
+++ examples/tests/token_test.rb
@@ -2,28 +2,22 @@ require 'minitest/autorun'
 require 'teth'
 
 class TokenTest < Teth::Minitest
-  def setup
-    @state = Tester::State.new
-    @solidity_code = File.read('./contracts/Token.sol')
-    @c = @state.abi_contract @solidity_code, language: :solidity
-  end
-
   def test_issue_balance
-    assert_equal 0, @c.getBalance(Tester::Fixture.accounts[2])
-    @c.issue Tester::Fixture.accounts[2], 100
-    assert_equal 100, @c.getBalance(Tester::Fixture.accounts[2])
+    assert_equal 0, contract.getBalance(address)
+    contract.issue address, 100
+    assert_equal 100, contract.getBalance(address)
   end
 
   def test_issue_exception
-    assert_raises(TransactionFailed) { @c.issue Tester::Fixture.accounts[3], 100, sender: Tester::Fixture.keys[4] }
-    assert_equal 0, @c.getBalance(Tester::Fixture.accounts[3])
+    assert_raises(TransactionFailed) { contract.issue Tester::Fixture.accounts[3], 100, sender: Tester::Fixture.keys[4] }
+    assert_equal 0, contract.getBalance(Tester::Fixture.accounts[3])
   end
 
   def test_token_transfer
-    @c.issue Tester::Fixture.accounts[2], 100
-    @c.transfer Tester::Fixture.accounts[3], 90, sender: Tester::Fixture.keys[2]
-    assert_equal 90, @c.getBalance(Tester::Fixture.accounts[3])
+    contract.issue Tester::Fixture.accounts[2], 100
+    contract.transfer Tester::Fixture.accounts[3], 90, sender: Tester::Fixture.keys[2]
+    assert_equal 90, contract.getBalance(Tester::Fixture.accounts[3])
 
-    assert_raises(TransactionFailed) { @c.transfer Tester::Fixture.accounts[3], 90, sender: Tester::Fixture.keys[2] }
+    assert_raises(TransactionFailed) { contract.transfer Tester::Fixture.accounts[3], 90, sender: Tester::Fixture.keys[2] }
   end
 end
--- lib/teth/minitest.rb
+++ lib/teth/minitest.rb
@@ -4,5 +4,103 @@ module Teth
   class Minitest < ::Minitest::Test
     include Ethereum
 
+    CONTRACT_DIR_NAME = 'contracts'
+
+    def setup
+      path = find_contract_source
+      raise "Cannot find corresponding contract source" unless path
+      setup_contract(path)
+    end
+
+    def teardown
+      @state = nil
+      @account = nil
+      @contract = nil
+    end
+
+    def setup_contract(path)
+      case path
+      when /\.sol\z/
+        code = File.read path
+        @contract = state.abi_contract code, language: :solidity
+      when /\.se\z/
+        raise NotImplemented, "Serpent not supported yet"
+      else
+        raise "Unknown contract source type: #{path}"
+      end
+    end
+
+    def contract
+      @contract
+    end
+
+    def find_contract_source
+      name = self.class.name[0...-4]
+      dir = find_contracts_directory
+      find_source(dir, name, 'sol') || find_source(dir, name, 'se')
+    end
+
+    def find_contracts_directory
+      last = nil
+      cur = ENV['PWD']
+      while cur != last
+        path = File.join cur, CONTRACT_DIR_NAME
+        return path if File.directory?(path)
+        last = cur
+        cur = File.dirname cur
+      end
+      nil
+    end
+
+    def find_source(dir, name, ext)
+      name = "#{name}.#{ext}"
+      list = Dir.glob File.join(dir, "**/*.#{ext}")
+      list.find {|fn| File.basename(fn) =~ /\A#{name}\z/i }
+    end
+
+    ##
+    # Fixtures
+    #
+
+    ACCOUNT_NUM = 10
+    @@privkeys = ACCOUNT_NUM.times.map do |i|
+      Utils.keccak256 rand(Constant::TT256).to_s
+    end
+
+    def privkey
+      account[0]
+    end
+
+    def pubkey
+      account[1]
+    end
+
+    def address
+      account[2]
+    end
+
+    def account
+      return @account if @account
+
+      i = rand(ACCOUNT_NUM)
+      @account = [privkeys[i], pubkeys[i], addresses[i]]
+    end
+
+    def privkeys
+      @privkeys ||= @@privkeys
+    end
+
+    def pubkeys
+      @pubkeys ||= privkeys.map {|k| PrivateKey.new(k).to_pubkey }
+    end
+
+    def addresses
+      @addresses ||= privkeys.map {|k| PrivateKey.new(k).to_address }
+    end
+
+    def state
+      @state ||= Tester::State.new
+    end
+
   end
 end
