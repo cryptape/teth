a101e86ece5bb33ab4848da966c1dbb0bbd4756d
u2 <zhangyaning1985@gmail.com>
2016-09-17 13:49:09 +0800

    Fixed geth test

--- lib/teth/templates/bin/test.sh
+++ lib/teth/templates/bin/test.sh
@@ -4,14 +4,19 @@ geth=${GETH:-geth}
 
 scripts=""
 
-for file in `find ./migrates -name '*.js'`
-do
-  scripts="${scripts};loadScript('$file')"
-done
-
-for file in `find ./tests -name '*.js'`
-do
-  scripts="${scripts};loadScript('$file');"
-done
+if [ -z "$1" ]
+then
+  for file in `find ./tests -name '*.js'`
+  do
+    scripts="${scripts};loadScript('$file');"
+  done
+  echo "Geth test all contracts ..."
+else
+  file="$1"
+  file="$(tr '[:lower:]' '[:upper:]' <<< ${file:0:1})${file:1}"
+  file+="_test.js"
+  scripts="loadScript('tests/$file');"
+  echo "Geth test $1"
+fi
 
 $geth --exec "$scripts" attach ipc:data/geth.ipc
