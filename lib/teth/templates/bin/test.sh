7b000eb7598bd96156d77b4fba8a96e7aaa2087a
u2 <zhangyaning1985@gmail.com>
2016-09-17 13:12:33 +0800

    Fixed geth test.sh

--- lib/teth/templates/bin/test.sh
+++ lib/teth/templates/bin/test.sh
@@ -4,15 +4,14 @@ geth=${GETH:-geth}
 
 scripts=""
 
-for file in `find ./builds -name '*compiled.js'`
+for file in `find ./migrates -name '*.js'`
 do
   scripts="${scripts};loadScript('$file')"
 done
 
-for file in `find ./test -name '*.js'`
+for file in `find ./tests -name '*.js'`
 do
   scripts="${scripts};loadScript('$file');"
 done
 
-echo $scripts
-$geth --datadir data --networkid 31415926 --rpc --rpccorsdomain "*" --nodiscover --unlock 3ae88fe370c39384fc16da2c9e768cf5d2495b48 --password <(echo -n 123456) --exec "$scripts" console 2>> ./logfile
+$geth --exec "$scripts" attach ipc:data/geth.ipc
