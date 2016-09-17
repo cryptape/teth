d074e44e0f359e12bc829aa9fa3ad7496ba7c82b
Jan Xie <jan.h.xie@gmail.com>
2016-09-17 19:26:27 +0800

    fix import key script

--- lib/teth/templates/private_keys/import.sh
+++ lib/teth/templates/private_keys/import.sh
@@ -6,6 +6,6 @@ set geth [lindex $argv 1];
 spawn $geth --datadir data account import $key
 expect "Passphrase:"
 send "123456\r"
-expect "Repeat Passphrase:"
+expect "Repeat passphrase:"
 send "123456\r"
 interact
